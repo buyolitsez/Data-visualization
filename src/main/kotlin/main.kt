1b0f70ec64f13a23e3d364f52217e23c5598a9ab
buyolitsez <hamerfall2003@yandex.ru>
2021-10-08 10:59:53 +0300

    Refactor main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,7 @@ import mu.KotlinLogging
 val logger = KotlinLogging.logger { }
 
 fun main() {
-    logger.info{"Start main"}
+    logger.info { "Start main" }
     readData()
     createWindow("pf-2021-viz")
     logger.info { "End main" }
