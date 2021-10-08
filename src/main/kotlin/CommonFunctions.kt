b4a184b7801dbe7d6f1103a4c9cfcb3a379ed250
buyolitsez <hamerfall2003@yandex.ru>
2021-10-08 10:19:07 +0300

    Introduce getRandomChar

--- src/main/kotlin/CommonFunctions.kt
+++ src/main/kotlin/CommonFunctions.kt
@@ -1,17 +1,10 @@
 import org.jetbrains.skija.Paint
 import kotlin.random.Random
 
-class myRandom {
-    val count = mutableMapOf<Int, Int>()
-    fun rnd(x : Int) : Int {
-//        if (!count.containsKey(x)) {
-//            count.set(x, 0)
-//        }
-        return Random(x).nextInt()
-    }
+fun getRandomChar(index: Int) : Int {
+    return Random(index).nextInt() % 256
 }
 
-val random = myRandom()
 
 fun distanceSq(x1: Float, y1: Float, x2: Float, y2: Float) = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
 
@@ -19,6 +12,6 @@ fun setColor(paint: Paint, index: Int) {
     if (index < COLORS_HEX.size) {
         paint.color = COLORS_HEX[index]
     } else {
-        paint.setARGB(random.rnd(index), random.rnd(index), random.rnd(index), random.rnd(index))
+        paint.setARGB(255, getRandomChar(index * 3), getRandomChar(index * 3 + 1), getRandomChar(index * 3 + 2))
     }
 }
