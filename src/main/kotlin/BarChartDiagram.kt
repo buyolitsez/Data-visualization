84a40e46bb447959acd7ad14393aa9128e3151b7
buyolitsez <hamerfall2003@yandex.ru>
2021-10-10 17:13:05 +0300

    split main function

--- src/main/kotlin/BarChartDiagram.kt
+++ src/main/kotlin/BarChartDiagram.kt
@@ -1,29 +1,47 @@
 import org.jetbrains.skija.*
 
+private const val leftDownY = WINDOW_HEIGHT - 150f
+private const val widthColumn = 30f
+private const val heightColumn = 400f
+private const val distanceBetweenTwoColumns = 30f
+
 fun displayBarChartDiagram(canvas: Canvas, paint: Paint) {
     logger.info { "display bar chart diagram" }
-    val widthColumn = 30f
-    val heightColumn = 400f
-    val distanceBetweenTwoColumns = 30f
     val maxValue = inputData.maxOf { it.value }
     var leftDownX = 70f
-    val leftDownY = WINDOW_HEIGHT - 150f
     for ((value, name) in inputData) {
-        // draw column
-        paint.color = BEAUTIFUL_BLUE
-        val currentHeight = (value / maxValue) * heightColumn // height of column
-        drawRectangle(
-            canvas, paint, arrayOf(
-                Point(leftDownX, leftDownY), // left down
-                Point(leftDownX + widthColumn, leftDownY), // right down
-                Point(leftDownX + widthColumn, leftDownY - currentHeight), // right up
-                Point(leftDownX, leftDownY - currentHeight) // left up
-            )
-        )
-        // draw name of column
-        paint.color = TEXT_COLOR
-        font.size = (distanceBetweenTwoColumns + widthColumn) / (name.length).toFloat()
-        canvas.drawString(name, leftDownX, leftDownY + font.size, font, paint) // draw name
+        drawColumn(canvas, paint, value, maxValue, leftDownX)
+        drawNameColumn(canvas, paint, name, leftDownX)
         leftDownX += widthColumn + distanceBetweenTwoColumns
     }
+}
+
+private fun drawNameColumn(
+    canvas: Canvas,
+    paint: Paint,
+    name: String,
+    leftDownX: Float
+) {
+    paint.color = TEXT_COLOR
+    font.size = (distanceBetweenTwoColumns + widthColumn) / (name.length).toFloat()
+    canvas.drawString(name, leftDownX, leftDownY + font.size, font, paint) // draw name
+}
+
+private fun drawColumn(
+    canvas: Canvas,
+    paint: Paint,
+    value: Float,
+    maxValue: Float,
+    leftDownX: Float,
+) {
+    paint.color = BEAUTIFUL_BLUE
+    val currentHeight = (value / maxValue) * heightColumn // height of current column
+    drawRectangle(
+        canvas, paint, arrayOf(
+            Point(leftDownX, leftDownY), // left down
+            Point(leftDownX + widthColumn, leftDownY), // right down
+            Point(leftDownX + widthColumn, leftDownY - currentHeight), // right up
+            Point(leftDownX, leftDownY - currentHeight) // left up
+        )
+    )
 }
