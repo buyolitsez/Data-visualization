2b8f4719dfd26f89084eaf9f1051e36c8eb0228e
buyolitsez <hamerfall2003@yandex.ru>
2021-10-10 16:15:38 +0300

    Add names for bar

--- src/main/kotlin/BarChartDiagram.kt
+++ src/main/kotlin/BarChartDiagram.kt
@@ -2,14 +2,16 @@ import org.jetbrains.skija.*
 
 fun displayBarChartDiagram(canvas: Canvas, paint: Paint) {
     logger.info { "display bar chart diagram" }
-    paint.setARGB(255, 14, 129, 234)
+    val fontSize = 20f
     val widthColumn = 30f
     val heightColumn = 400f
     val distanceBetweenTwoColumns = 30f
+    font.size = fontSize
     val maxValue = inputData.maxOf { it.value }
     var leftDownX = 5f
     val leftDownY = WINDOW_HEIGHT - 150f
-    for ((value, _) in inputData) {
+    for ((value, name) in inputData) {
+        paint.setARGB(255, 14, 129, 234)
         val currentHeight = (value / maxValue) * heightColumn
         drawSquare(
             canvas, paint, arrayOf(
@@ -19,6 +21,9 @@ fun displayBarChartDiagram(canvas: Canvas, paint: Paint) {
                 Point(leftDownX, leftDownY - currentHeight)
             )
         )
+        paint.color = 0xff2C2828.toInt()
+        canvas.drawString(name, leftDownX, leftDownY + fontSize, font, paint)
         leftDownX += widthColumn + distanceBetweenTwoColumns
+
     }
 }
