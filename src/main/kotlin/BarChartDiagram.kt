7e5c9b009acaea1a8cc5757cea90000344baf2ce
buyolitsez <hamerfall2003@yandex.ru>
2021-10-10 17:06:35 +0300

    refactor BarChartDiagram.kt

--- src/main/kotlin/BarChartDiagram.kt
+++ src/main/kotlin/BarChartDiagram.kt
@@ -6,22 +6,24 @@ fun displayBarChartDiagram(canvas: Canvas, paint: Paint) {
     val heightColumn = 400f
     val distanceBetweenTwoColumns = 30f
     val maxValue = inputData.maxOf { it.value }
-    var leftDownX = 5f
+    var leftDownX = 70f
     val leftDownY = WINDOW_HEIGHT - 150f
     for ((value, name) in inputData) {
+        // draw column
         paint.color = BEAUTIFUL_BLUE
-        val currentHeight = (value / maxValue) * heightColumn
-        drawSquare(
+        val currentHeight = (value / maxValue) * heightColumn // height of column
+        drawRectangle(
             canvas, paint, arrayOf(
-                Point(leftDownX, leftDownY),
-                Point(leftDownX + widthColumn, leftDownY),
-                Point(leftDownX + widthColumn, leftDownY - currentHeight),
-                Point(leftDownX, leftDownY - currentHeight)
+                Point(leftDownX, leftDownY), // left down
+                Point(leftDownX + widthColumn, leftDownY), // right down
+                Point(leftDownX + widthColumn, leftDownY - currentHeight), // right up
+                Point(leftDownX, leftDownY - currentHeight) // left up
             )
         )
+        // draw name of column
         paint.color = TEXT_COLOR
         font.size = (distanceBetweenTwoColumns + widthColumn) / (name.length).toFloat()
-        canvas.drawString(name, leftDownX, leftDownY + font.size, font, paint)
+        canvas.drawString(name, leftDownX, leftDownY + font.size, font, paint) // draw name
         leftDownX += widthColumn + distanceBetweenTwoColumns
     }
 }
